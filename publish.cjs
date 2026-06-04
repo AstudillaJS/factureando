21d0dca323c593860dcc9b8c5bc31791ecc539be
AstudillaJS <astudillo.js@gmail.com>
2026-06-04 11:54:57 -0300

    chore: agregar script npm run publish y soporte para dotenv

--- publish.cjs
+++ publish.cjs
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const { execSync } = require('child_process');
-// process.env.GH_TOKEN should be set in the shell environment before running
+
 try {
   execSync('npm run release', { stdio: 'inherit' });
 } catch (e) {
